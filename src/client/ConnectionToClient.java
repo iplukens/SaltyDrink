4a5036b2733a76edb5bd194636de40c467965d9d
iplukens <ian@botch.com>
2014-08-26 23:30:13 -0400

    updated pom with java version; added error handling for client disconnect (still needs work)

--- src/client/ConnectionToClient.java
+++ src/client/ConnectionToClient.java
@@ -10,6 +10,7 @@ import server.Server;
 public class ConnectionToClient extends Thread {
 
 	private Server server;
+	private Socket client;
 	private ObjectInputStream in;
 	private ObjectOutputStream out;
 	private MessageHandler handler;
@@ -18,16 +19,30 @@ public class ConnectionToClient extends Thread {
 		in = new ObjectInputStream(client.getInputStream());
 		out = new ObjectOutputStream(client.getOutputStream());
 		handler = new MessageHandler();
+		this.client = client;
 	}
 
 	public void run() {
 		Object message;
-		while ((message = in) != null) {
-
-			Object response = handler.process(message);
-			send(response);
+		try {
+			while ((message = in.readObject()) != null) {
+				System.out.println(message);
+				Object response = handler.process(message);
+				send(response);
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		server.remove(this);
+		try {
+			in.close();
+			out.close();
+			client.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
